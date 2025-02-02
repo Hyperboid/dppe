e24a580dd7d1388de8efe56f8ed601869fbe1e46
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-02-02 17:22:22 -0500

    Somewhat proper color picker

--- scripts/objects/PaletteEditorColorPicker.lua
+++ scripts/objects/PaletteEditorColorPicker.lua
@@ -18,25 +18,50 @@ end
 
 function PaletteEditorColorPicker:onEnter(_, color_ref)
     self.color_ref = color_ref
+    self.channel_selected = 1
 end
 
 function PaletteEditorColorPicker:onKeyPressed(key)
     if Input.is("cancel", key) then
         self.editor:popState()
         Assets.playSound("ui_cancel_small")
+    elseif Input.is("down", key) then
+        self.channel_selected = (self.channel_selected % 3)+1
+        Assets.stopAndPlaySound("ui_move")
+    elseif Input.is("up", key) then
+        Assets.stopAndPlaySound("ui_move")
+        self.channel_selected = ((self.channel_selected-2) % 3)+1
     end
 end
 
 function PaletteEditorColorPicker:update()
     local color = self.color_ref
-    if Input.down("up") then
-        color[1] = .5
+    local incdec = DT*.3
+    if Input.down("right") then
+        color[self.channel_selected] = Utils.clamp(color[self.channel_selected]+incdec, 0,1)
+    elseif Input.down("left") then
+        color[self.channel_selected] = Utils.clamp(color[self.channel_selected]-incdec, 0,1)
     end
 end
 
-
 function PaletteEditorColorPicker:draw()
-    
+    love.graphics.push()
+    love.graphics.setColor(self.color_ref)
+    love.graphics.translate(0,SCREEN_HEIGHT-100)
+    love.graphics.rectangle("fill", 0,0,SCREEN_WIDTH,100)
+    local w = SCREEN_WIDTH
+    love.graphics.translate(0,-20)
+    Draw.setColor({0.5,0,0})
+    if self.channel_selected == 1 then Draw.setColor({1,0,0}) end
+    love.graphics.setLineWidth(8)
+    love.graphics.line(0,0,self.color_ref[1]*w,0)
+    Draw.setColor(COLORS.green)
+    if self.channel_selected == 2 then Draw.setColor(COLORS.lime) end
+    love.graphics.line(0,8,self.color_ref[2]*w,8)
+    Draw.setColor({0,0,0.5})
+    if self.channel_selected == 3 then Draw.setColor({0,0,1}) end
+    love.graphics.line(0,16,self.color_ref[3]*w,16)
+    love.graphics.pop()
 end
 
 return PaletteEditorColorPicker
