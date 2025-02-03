f62590afdd340982de7f1c6662b874d7b55198c2
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-02-02 21:21:36 -0500

    Color picker but better

--- scripts/objects/ColorPicker.lua
+++ scripts/objects/ColorPicker.lua
@@ -5,14 +5,38 @@ local COLOR_SQUARE_X_OFFSET = 100
 local COLOR_SQUARE_Y_OFFSET = 100
 local HUE_X_OFFSET = 400
 local HUE_Y_OFFSET = 100
+
+
+local function rgbToHsv(r, g, b, a)
+    local max, min = math.max(r, g, b), math.min(r, g, b)
+    local h, s, v
+    v = max
+
+    local d = max - min
+    if max == 0 then s = 0 else s = d / max end
+
+    if max == min then
+        h = 0 -- achromatic
+    else
+        if max == r then
+        h = (g - b) / d
+        if g < b then h = h + 6 end
+        elseif max == g then h = (b - r) / d + 2
+        elseif max == b then h = (r - g) / d + 4
+        end
+        h = h / 6
+    end
+  
+    return h, s, v, a
+end
+
 function ColorPicker:init(color,x,y)
-    super.init(self,x,y,256+64,256)
+    super.init(self,x,y,256+32+4,256)
     self.color_square_canvas = love.graphics.newCanvas(256, 256)
 
     self.hue_canvas = love.graphics.newCanvas(32, 256)
 
-    self.hue, self.saturation, self.value = Utils.rgbToHsl(unpack(color))
-    self.value = self.value * 2
+    self.hue, self.saturation, self.value = rgbToHsv(unpack(color))
 
     self.resolution = (1/64)
     self.on_edit = function() end
